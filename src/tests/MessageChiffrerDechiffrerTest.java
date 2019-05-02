2306084b601631d135a42b54f780ea258c886968
DesMetM <dylanrenaud2013@gmail.com>
2019-05-02 16:56:16 -0400

    ajout des deux classe

---
+++ src/tests/MessageChiffrerDechiffrerTest.java
@@ -0,0 +1,52 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageChiffrerDechiffrerTest
+{
+ pas encore fait
+	@Test
+	public void testMessageChiffrerDechiffrer()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMatriceCourante()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValiderMessageSelonDico()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAjusterMessageBrute()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEncoder()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDecoder()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain()
+	{
+		fail("Not yet implemented");
+	}
+
+}
